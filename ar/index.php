0572ab2300a5e36f9f470e6b07b62ff936d5777e
seif-elden <sofa6099@gmail.com>
2021-06-07 01:26:29 +0200

    remove contact from index

--- ar/index.php
+++ ar/index.php
@@ -116,7 +116,7 @@ if (isset($_COOKIE['user'])) {
           <li><a class="nav-link scrollto" href="#services">خدماتنا</a></li>
           <li><a class="nav-link scrollto" href="#portfolio">معرض اعمالنا</a></li>
           <li><a class="nav-link scrollto " href="#technologies">التقنيات</a></li>
-          <li><a class="nav-link scrollto" href="#contact">تواصل معنا</a></li>
+          <li><a class="nav-link scrollto" href="contact.php">تواصل معنا</a></li>
           <?php
 
           if (isset($_COOKIE['user'])) {
@@ -645,76 +645,6 @@ if (isset($_COOKIE['user'])) {
 
   </main><!-- End #main -->
 
-    <!-- ======= Contact Section ======= -->
-
-  <section id="contact" class="contact section-bg" dir="rtl">
-    <div class="container" data-aos="fade-up">
-
-      <div class="section-title">
-        <h2>تواصل</h2>
-        <p> تواصل معنا</p>
-      </div>
-
-      <div class="row">
-
-        <div class="col-lg-6">
-
-          <div class="row">
-            <div class="col-md-12">
-              <div class="info-box">
-                <i class="fas fa-map-marker-alt"></i>
-                <h3> عنواننا </h3>
-                <p>المملكة العربية السعودية - الرياض
-                  - طريق الملك فهد - برج حمد </p>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="info-box mt-4">
-                <i class="fas fa-envelope"></i>
-                <h3> بريدنا الاكتروني</h3>
-                <p>info@rightaccess.co<br></p>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="info-box mt-4">
-                <i class="fas fa-phone-alt"></i>
-                <h3> اتصل بنا</h3>
-                <p>966550978623+<br></p>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-        <div class="col-lg-6">
-          <form action="https://vuinteriordesign.pythonanywhere.com/contact/21/" method="post" role="form" class="php-email-form">
-            <div class="row">
-              <div class="col-md-6 form-group">
-                <input type="text" name="name" class="form-control" id="name" value="<?php echo $name; ?>" placeholder=" اسمك" required>
-              </div>
-              <div class="col-md-6 form-group mt-3 mt-md-0">
-                <input type="email" class="form-control" name="email" id="email" value="<?php echo $email; ?>" placeholder=" بريدك الاكتروني" required>
-              </div>
-            </div>
-            <div class="form-group mt-3">
-              <input type="text" class="form-control" name="subject" id="subject" placeholder="عنوان" required>
-            </div>
-            <div class="form-group mt-3">
-              <textarea class="form-control" name="message" rows="5" placeholder="رسالتك" required></textarea>
-            </div>
-            <div class="my-3">
-              <div class="loading">تحميل</div>
-              <div class="error-message"></div>
-              <div class="sent-message">تم ارسال رسالتك بنجاح  .. شكرا لك !</div>
-            </div>
-            <div class="text-center"><button type="submit"> ارسال</button></div>
-          </form>
-        </div>
-
-      </div>
-
-    </div>
-  </section><!-- End Contact Section -->
 
   <!-- ======= Footer ======= -->
   <footer id="footer" dir="rtl">
--- index.php
+++ index.php
@@ -81,7 +81,7 @@ if (isset($_COOKIE['user'])) {
           <li><a class="nav-link scrollto" href="#services">Services</a></li>
           <li><a class="nav-link scrollto" href="#portfolio">projects</a></li>
           <li><a class="nav-link scrollto " href="#technologies">technologies</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="nav-link scrollto" href="contact.php">Contact</a></li>
           <?php
 
           if (isset($_COOKIE['user'])) {
@@ -609,74 +609,7 @@ if (isset($_COOKIE['user'])) {
     </section>
     <!-- end technologies section -->
 
-    <!-- ======= Contact Section ======= -->
-    <section id="contact" class="contact section-bg">
-      <div class="container" data-aos="fade-up">
 
-        <div class="section-title">
-          <h2>Contact</h2>
-          <p>Contact Us</p>
-        </div>
-
-        <div class="row">
-
-          <div class="col-lg-6">
-
-            <div class="row">
-              <div class="col-md-12">
-                <div class="info-box">
-                  <i class="fas fa-map-marker-alt"></i>
-                  <h3>Our Address</h3>
-                  <p> Saudi Arabia - Riyadh - King Fahd Road - Hamad Tower</p>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="info-box mt-4">
-                  <i class="fas fa-envelope"></i>
-                  <h3>Email Us</h3>
-                  <p>info@rightaccess.co<br></p>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="info-box mt-4">
-                  <i class="fas fa-phone-alt"></i>
-                  <h3>Call Us</h3>
-                  <p>+ 966550978623<br></p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-          <div class="col-lg-6">
-            <form action="https://vuinteriordesign.pythonanywhere.com/contact/21/" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" value="<?php echo $name; ?>" placeholder="Your Name" required>
-                </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" value="<?php echo $email; ?>" placeholder="Your Email" required>
-                </div>
-              </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Contact Section -->
 
   </main><!-- End #main -->
 
